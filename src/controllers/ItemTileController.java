05c999b1d91a3b24f73bb957727cfbfd18ca4391
Sebastian Nilsson <sebbehebbe@gmail.com>
2016-02-23 15:53:04 +0100

    Connected product to tile

--- src/controllers/ItemTileController.java
+++ src/controllers/ItemTileController.java
@@ -2,9 +2,15 @@ package controllers;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import se.chalmers.ait.dat215.project.IMatDataHandler;
+import se.chalmers.ait.dat215.project.Product;
+import se.chalmers.ait.dat215.project.ShoppingCart;
+import se.chalmers.ait.dat215.project.ShoppingItem;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -14,12 +20,26 @@ import java.util.ResourceBundle;
  */
 public class ItemTileController implements Initializable {
     @FXML Label title, price;
+    @FXML Button addProductBtn;
     @FXML ImageView image;
+    @FXML TextField amountField;
+    private Product product;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
     }
+
+    @FXML
+    protected void addProductToCart() {
+        IMatDataHandler im = IMatDataHandler.getInstance();
+        ShoppingCart sc = im.getShoppingCart();
+        int amount = Integer.parseInt(amountField.getText());
+        sc.addProduct(this.product, amount);
+        sc.fireShoppingCartChanged(new ShoppingItem(this.product, amount), true);
+    }
+
+    public void setProduct(Product p) {}
 
     public void setTitle(String title){
         this.title.setText(title);

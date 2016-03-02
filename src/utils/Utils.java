87f4aafcd4cb7abe18b9fe3931cec46b199460b4
Sebastian Nilsson <sebastian@winternet.se>
2016-03-02 22:16:00 +0100

    My most bought products finished

--- src/utils/Utils.java
+++ src/utils/Utils.java
@@ -3,9 +3,11 @@ package utils;
 import javafx.scene.control.TextField;
 import se.chalmers.ait.dat215.project.Product;
 
+import java.lang.reflect.Array;
 import java.math.RoundingMode;
 import java.text.DecimalFormat;
 import java.text.DecimalFormatSymbols;
+import java.util.*;
 
 /**
  * By: Sebastian Nilsson
@@ -50,6 +52,28 @@ public class Utils {
         df.setRoundingMode(RoundingMode.CEILING);
 
         return df.format(price);
+    }
+
+    public static <K, V extends Comparable<? super V>> Map<K, V> sortByValue( Map<K, V> map ) {
+        List<Map.Entry<K, V>> list =
+                new LinkedList<Map.Entry<K, V>>( map.entrySet() );
+        Collections.sort( list, new Comparator<Map.Entry<K, V>>()
+        {
+            public int compare( Map.Entry<K, V> o1, Map.Entry<K, V> o2 )
+            {
+                return (o1.getValue()).compareTo( o2.getValue() );
+            }
+        } );
+
+        Collections.reverse(list);
+
+        Map<K, V> results = new LinkedHashMap<>();
+        for (Map.Entry<K,V> entry: list) {
+            results.put(entry.getKey(), entry.getValue());
+        }
+
+        return results;
+
     }
 
 }

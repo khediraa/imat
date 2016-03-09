05f8af1d6dc1a14b96ea43eabf9d8c4a202fce29
Tuyen Ngo <tuyen@student.chalmers.se>
2016-03-09 21:34:37 +0100

    Attempt at receipts.

--- src/controllers/ConfirmationController.java
+++ src/controllers/ConfirmationController.java
@@ -1,19 +1,22 @@
 package controllers;
 
 import imat.IObservable;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import se.chalmers.ait.dat215.project.CartEvent;
-import se.chalmers.ait.dat215.project.ShoppingCartListener;
+import javafx.scene.control.ListView;
+import se.chalmers.ait.dat215.project.*;
 
 import javax.xml.soap.Text;
 import java.beans.PropertyChangeListener;
 import java.beans.PropertyChangeSupport;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
 
 /**
@@ -27,6 +30,10 @@ public class ConfirmationController implements Initializable, ShoppingCartListen
     @FXML private Label dateConfirm;
     @FXML private Label timeConfirm;
     @FXML private Label deliveryLocationText;
+    @FXML private ListView receiptList;
+    private IMatDataHandler dataInstance = IMatDataHandler.getInstance();
+    private List<Order> orders = new ArrayList<>();
+    private List<ShoppingItem> latestOrders = new ArrayList<>();
 
     private final PropertyChangeSupport pcs = new PropertyChangeSupport(this);
 
@@ -38,6 +45,13 @@ public class ConfirmationController implements Initializable, ShoppingCartListen
                 pcs.firePropertyChange("to-main-page", true, false);
             }
         });
+
+        if (dataInstance.getOrders().size() > 0) {
+            orders.addAll(dataInstance.getOrders());
+            Order lastOrder = orders.get(orders.size() - 1);
+
+            lastOrder.getItems().forEach(item -> System.out.println(item.getProduct().getName()));
+        }
     }
 
     public void showDeliveryTime(String time, String date, boolean isHomeDelivery){
